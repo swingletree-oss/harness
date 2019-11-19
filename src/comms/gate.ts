be4a548762ba2c34ddb4110f43d6e301e2e289ee
Michael Gerbig <error418@users.noreply.github.com>
2019-11-19 22:09:56 +0100

    feat: add buildUuid property to gate plugin call model

--- src/comms/gate.ts
+++ src/comms/gate.ts
@@ -1,13 +1,19 @@
 import { Harness } from "../model";
 import { WithMetaMessage } from "./message";
 
+/** Report Data sent by Gate to a specific plugin */
 export interface PluginReportProcessData<D> {
+  /** Swingletree Headers received by Gate */
   headers: Map<string, string>;
+  /** report received by Gate */
   report: D;
 }
 
 export interface PluginReportProcessMetadata {
+  /** scm source information */
   source: Harness.ScmSource;
+  /** uuid of the build */
+  buildUuid: string;
 }
 
 export class PluginReportProcessRequest<D extends Object> implements WithMetaMessage<PluginReportProcessData<D>, PluginReportProcessMetadata> {
